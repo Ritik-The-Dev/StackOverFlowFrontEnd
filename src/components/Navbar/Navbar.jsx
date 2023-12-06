bafdb3b6648acdd06afc931bb7efac2d1dcecaad
Ritik <joshiritik865@gmail.com>
2023-12-06 21:45:24 +0530

    Update Local Storags

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -34,7 +34,6 @@ const Navbar = ({ handleSlideIn }) => {
     if (token) {
       const decodedToken = decode(token);
       if (decodedToken.exp * 1000 < new Date().getTime()) {
-        handleLogout();
       }
     }
     dispatch(setCurrentUser(JSON.parse(localStorage.getItem("Profile"))));
